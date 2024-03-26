434dcdcc3684032185e74c3477fb9c8f602cc6d6
Thomas <123456.max.thomas@gmail.com>
2024-03-26 17:36:55 -0400

    login and sign up pages are now available for use

--- app.py
+++ app.py
@@ -19,6 +19,10 @@ def roommate():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 @app.route('/logout')
 def logout():
     #Should log the user out and return to homepage
